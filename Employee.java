38c6e940fbb56fdf1013f11d9621bdb5494491bd
AachalDanole <138237840+AachalDanole@users.noreply.github.com>
2024-10-07 15:46:11 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,25 +1,80 @@
-package Task_managementService_interface;
+package TaskManagement_Service;
+import java.sql.Date;
 
-import java.util.Optional;
+import org.hibernate.annotations.DynamicUpdate;
 
-import org.springframework.stereotype.Service;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
-@Service
-public interface Employee {
+@Entity
+@Table(name = "Employee")
+@DynamicUpdate
 
+public class Employee {
 
-	Employee saveEmployee(Employee employee);
 	
-	Optional<Employee> getTaskById(int empId);
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int employee_id;
+	
+	@Column(name="employee_name",nullable = false)
+	String employee_name;
+	
+	@Column(name="employee_mail",nullable = false)
+	String employee_mail;
+	
+	@Column(name="password",nullable = false)
+	String password;
+	
+	@Column(name="employee_name",nullable = false)
+	String task;
+	
+	@Column(name="dueDate",nullable = false)
+	Date dueDate;
 	
 	
-
 	
-
+	public String getEmployee_name() {
+		return employee_name;
+	}
+	public void setEmployee_name(String employee_name) {
+		this.employee_name = employee_name;
+	}
+	public String getEmployee_mail() {
+		return employee_mail;
+	}
+	public void setEmployee_mail(String employee_mail) {
+		this.employee_mail = employee_mail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getTask() {
+		return task;
+	}
+	
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public Date getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [employee_id=" + employee_id + ", employee_name=" + employee_name + ", employee_mail="
+				+ employee_mail + ", password=" + password + ", task=" + task + ", dueDate=" + dueDate + "]";
+	}
+	
 }
-
-
-
-
--- Manager.java
+++ Manager.java
@@ -1,31 +1,94 @@
-package Task_managementService_interface;
+package TaskManagement_Service;
+import java.sql.Date;
 
-import java.util.List;
-import java.util.Optional;
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
-import TaskManagement_Service.Employee;
+@Entity
+@Table(name = "Manager")
+@DynamicUpdate
 
+public class Manager {
 
+	@Column(name="manager_name",nullable = false)
 
-public interface Manager {
+	private String  manager_name;
 	
+	@Column(name="manager_mail",nullable = false)
+
+	private String manager_mail;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="password",nullable = false)
+
+	private String password;
+	
+	@Column(name="task",nullable = false)
+	private String task;
+	
+	@Column(name="dateOfSubmission",nullable = false)
+	private Date dateOfSubmission;
 
 	
-	Employee saveempDetails(Employee emp);
+	//setters and getters methods
+	public String getManager_name() {
+		return manager_name;
+	}
+	public void setManager_name(String manager_name) {
+		this.manager_name = manager_name;
+	}
+	public String getManager_mail() {
+		return manager_mail;
+	}
+	public void setManager_mail(String manager_mail) {
+		this.manager_mail = manager_mail;
+	}
 	
-    Optional<Employee> getDetailsById(int empIde);
-
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public Date getDateOfSubmission() {
+		return dateOfSubmission;
+	}
+	public void setDateOfSubmission(Date dateOfSubmission) {
+		this.dateOfSubmission = dateOfSubmission;
+	}
+	
+	public Manager(String managername,String managermail,String task,String password)
+	{
+		this.manager_mail=managermail;
+		this.manager_name=managername;
+		this.task=task;
+		this.password=password;
+		
+		
+	}
+	@Override
+	public String toString() {
+		return "Manager [manager_name=" + manager_name + ", manager_mail=" + manager_mail + ", id=" + id + ", password="
+				+ password + ", task=" + task + ", dateOfSubmission=" + dateOfSubmission + "]";
+	}
 	
 	
 	
-	// Method to update an existing Bus entity in the database based on its ID
-	Manager updateTaskById( Optional<Employee> empDet,Employee EmpDetails);
-	
-	// Method to delete a specific Bus entity from the database based on its ID
-	void deleteTaskById(int empId);
-	
-	
-
 }
